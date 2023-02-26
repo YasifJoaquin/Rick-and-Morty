cfb91f9161e90fd160b696e5e842bc755c021472
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-25 18:07:14 -0600

    retoques de las vistas, falta datos personales y personajes

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -9,7 +9,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
 <template>
   <div class="border border-double border-black w-3/5 h-screen flex justify-center items-center">
-    <div class="border border-double border-yellow w-3/5 rounded-lg text-center">
+    <div class="border border-double border-yellow w-3/5 rounded-lg text-center bg-gray-800">
       <p class="text-green-500 text-5xl font-bold pt-6">Rick and Morty</p>
       <p class="mt-3 p-5 text-justify text-xl">
         Después de haber estado desaparecido durante casi 20 años, Rick Sanchez llega repentinamente

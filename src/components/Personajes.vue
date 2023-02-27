b224a13a7459f7ecc429a0d547dae376e8adb7ab
YasifJoaquin <yasif.19070002@itsmotul.edu.mx>
2023-02-27 15:57:03 -0600

    busqueda, botones de pagina y detalles de personajas ya funciona. pero con detalles

--- src/components/Personajes.vue
+++ src/components/Personajes.vue
@@ -11,6 +11,8 @@ export default {
       personajes: [],
       cont:2,
       search: "",
+      dato:"",
+      detalle_personajes: []
     }
   },
 
@@ -44,7 +46,31 @@ export default {
         this.info = response.data.info;
         this.personajes = response.data.results;
       })
-      this.cont == this.cont--
+      this.cont--
+    },
+
+    buscando(search) {
+      API_URL='https://rickandmortyapi.com/api/character/?name='+search
+      console.log(API_URL)
+      axios.get(API_URL)
+      .then((response) => {
+        console.log(response.config)
+        this.info = response.data.info;
+        this.personajes = response.data.results;
+      })
+    },
+
+    datosp(id) {
+      API_URL='https://rickandmortyapi.com/api/character/'+id
+      console.log(API_URL)
+      axios.get(API_URL)
+      .then((response) => {
+        console.log(response.config)
+        this.detalle_personajes = response.data;
+        console.log(this.detalle_personajes)
+      })
+      this.cont++
+
     }
   }
 
@@ -55,16 +81,34 @@ export default {
 <template bg-red-400>
   <div class="flex justify-center">
   <button v-if="cont > 2" @click="prev(cont)" class="border border-lime-400 bg-teal-600 px-3 text-2xl text-black font-medium rounded-l-lg m-3"> Prev </button>
-  <input type="text" v.model="search" class="border border-teal-600 bg-lime-500 px-3 text-xl text-black font-medium rounded-full m-3">
+  <input type="text" v-model="search" @input="buscando(search)" class="border border-teal-600 bg-lime-500 px-3 text-xl text-black font-medium rounded-full m-3">
   <button v-if="cont < 43" @click="pag(cont)" class="border border-lime-400 bg-teal-600 px-3 text-2xl text-black font-medium rounded-r-lg m-3"> Next </button>
   </div>
 
-  <div class="border border-lime-500 w-2/5 h-screen text-center">
+  <div class="flex">
+    <div class="border border-lime-500 w-2/5 h-screen text-center">
     <h2>pagina siguiente {{ cont }}</h2>
     <ul class="list-disc">
       <li v-for="p in personajes">
-        <a >{{ p.name }} id:{{ p.id }}</a> 
+        <button @click="datosp(p.id)">{{ p.name }} id:{{ p.id }}</button> 
       </li>
     </ul>
+  </div >
+
+  <ul>
+    <li>
+      <div class="bg-white shadow-md rounded-lg max-w-sm">
+        <img class="rounded-t-lg" v-bind:src="detalle_personajes.image" alt="">
+        <div class="p-5 bg-lime-500">
+          <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2"> {{ detalle_personajes.name }} </h5>
+          <p class="font-normal text-gray-700 text-justify">Estado: {{ detalle_personajes.status }}</p>
+          <p class="font-normal text-gray-700 text-justify">Especie: {{ detalle_personajes.species }}</p>
+          <p class="font-normal text-gray-700 text-justify">Genero: {{ detalle_personajes.gender }}</p>
+        </div>
+      </div>
+    </li>
+  </ul>
   </div>
+
 </template>
+
